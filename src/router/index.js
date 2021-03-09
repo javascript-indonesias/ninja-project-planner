e8fa5c4ce50fa437bbc81432d3b348808fc95b90
GulajavaMinistudio <gulajava.mini@gmail.com>
2021-03-09 17:12:04 +0700

    Pembuatan awal halaman homepage

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 // route level code-splitting
 // this generates a separate chunk (about.[hash].js) for this route
 // which is lazy-loaded when the route is visited.
+const HomePageComponent = () => import(/* webpackChunkName: "homepage" */ '../views/Homepage.vue');
 const HomeComponent = () => import(/* webpackChunkName: "home" */ '../views/About.vue');
 const AboutComponent = () => import(/* webpackChunkName: "about" */ '../views/About.vue');
 
 const routes = [
     {
         path: '/',
+        name: 'Homepage',
+        component: HomePageComponent,
+    },
+    {
+        path: '/homes',
         name: 'Home',
         component: HomeComponent,
     },
---
+++ src/views/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+        Home
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss"></style>
