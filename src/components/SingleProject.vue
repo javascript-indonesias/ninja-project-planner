59e5cd1ee07e5e4b944a988df6034a63d2e96b9d
GulajavaMinistudio <gulajava.mini@gmail.com>
2021-03-11 02:33:22 +0700

    Update halaman single project dan props component

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="project">
-        <div class="action"></div>
+        <div class="action">
+            <h3 @click="showDetails()">{{ dataProject.title }}</h3>
+        </div>
+
+        <div class="details" v-if="isDetailShow">
+            <p>{{ dataProject.details }}</p>
+        </div>
     </div>
 </template>
 
@@ -20,10 +26,40 @@ export default {
             },
         },
     },
+    data() {
+        return {
+            dataProject: this.project,
+            isDetailShow: false,
+        };
+    },
     setup() {
         return {};
+    },
+    methods: {
+        showDetails() {
+            this.isDetailShow = !this.isDetailShow;
+        },
+    },
+    watch: {
+        project(newValue, oldValue) {
+            if (newValue !== oldValue) {
+                this.dataProject = newValue;
+            }
+        },
     },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.project {
+    margin: 20px auto;
+    background: #fff;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+    border-left: 4px solid #e90074;
+}
+h3 {
+    cursor: pointer;
+}
+</style>
