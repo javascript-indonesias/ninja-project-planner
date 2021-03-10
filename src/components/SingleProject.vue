13c0680cdd8103630923387d65d2ac13c4af0cb2
GulajavaMinistudio <gulajava.mini@gmail.com>
2021-03-10 12:58:02 +0700

    Pembuatan list multi component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="project">
+        <div class="action"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SingleProjectComponent',
+    props: {
+        project: {
+            type: Object,
+            default() {
+                return {
+                    id: 1,
+                    title: 'Empty project',
+                    details: 'empty project not available',
+                    complete: false,
+                };
+            },
+        },
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss"></style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,13 +1,60 @@
 <template>
     <div class="home">
-        Home
+        <div v-if="listPlanner.length > 0">
+            <div v-for="planner in listPlanner" :key="planner.id">
+                <single-project :project="planner"></single-project>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
+
+const SingleProjectComponent = defineAsyncComponent(() =>
+    import('../components/SingleProject.vue'),
+);
+
 export default {
+    name: 'HomepageComponent',
+    components: {
+        'single-project': SingleProjectComponent,
+    },
     setup() {
         return {};
+    },
+    data() {
+        return {
+            listPlanner: [],
+        };
+    },
+    methods: {
+        getDataStarterProjectPlanner() {
+            fetch('http://localhost:3000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                },
+            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        return response.json();
+                    }
+                    return Promise.resolve(
+                        new Error(`Response gagal dengan status ${response.text}`),
+                    );
+                })
+                .then((result) => {
+                    console.log(result);
+                    this.listPlanner = result;
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        },
+    },
+    mounted() {
+        this.getDataStarterProjectPlanner();
     },
 };
 </script>
